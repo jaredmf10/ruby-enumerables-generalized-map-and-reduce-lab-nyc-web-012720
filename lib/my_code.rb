0258fdbb2aa9b8f18f13925ba62f1e4bec62745a
Jared Fleming <jaredmf10@gmail.com>
2020-01-23 17:58:14 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,8 +9,17 @@ i = 0
   new
 end
 
-def reduce(array)
-new = []
-i = 0
-
+def reduce(array, sv=nil)
+  if sv 
+    sum = sv
+    i = 0
+  else
+    sum = array[0]
+    i = 1
+  end
+  while i < array.length
+    sum = yield(sum, array[i])
+    i += 1
+  end
+  sum
 end
